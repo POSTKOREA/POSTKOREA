fc4550ea021d34a92d2da967313541c9a8e31992
Dongyoung <doyokim75@gmail.com>
2024-01-23 17:56:55 +0900

    feat, test : "로그인 fragment 생성, git test"
    로그인 fragment 생성, git test

---
+++ Android/app/src/main/java/com/ssafy/travelcollector/LoginFragment.kt
@@ -0,0 +1,14 @@
+package com.ssafy.travelcollector
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.travelcollector.config.BaseFragment
+import com.ssafy.travelcollector.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::bind, R.layout.fragment_login) {
+
+
+}
