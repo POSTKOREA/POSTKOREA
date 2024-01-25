f1e24e0e6651ea3685a754202d1f9a2393e1b073
김건우 <rlarjsdn0407@naver.com>
2024-01-25 16:58:18 +0900

    feat : heritageList fragment
    문화유산 리스트형 목록 fragment xml 생성
    
    Change-Id: Ie2153765cdb349024285ec7ed4d8d98e134c8818

---
+++ Android/app/src/main/java/com/ssafy/travelcollector/HeritageListFragment.kt
@@ -0,0 +1,15 @@
+package com.ssafy.travelcollector
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.travelcollector.config.BaseFragment
+import com.ssafy.travelcollector.databinding.FragmentHeritageListBinding
+
+class HeritageListFragment : BaseFragment<FragmentHeritageListBinding>(FragmentHeritageListBinding::bind, R.layout.fragment_heritage_list){
+
+
+
+}
