f5bc515d64c1513c7a7689fd12eb963022a224d1
ukyoung147 <ukyoung147@gmail.com>
2024-01-31 17:21:45 +0900

    Fix: "Swagger 어노테이션 추가"

--- BackEnd/dmobile/src/main/java/com/ssafy/dmobile/relic/controller/RelicController.java
+++ BackEnd/dmobile/src/main/java/com/ssafy/dmobile/relic/controller/RelicController.java
@@ -5,6 +5,8 @@ import com.ssafy.dmobile.relic.entity.ListData;
 import com.ssafy.dmobile.relic.repository.DetailDataRepository;
 import com.ssafy.dmobile.relic.repository.ListDataRepository;
 //import com.ssafy.dmobile.service.DetailDataService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController // 이건 json이랑 xml 받을때
 // rest api로 통신
 @RequestMapping("/relic")
+@Tag(name = "Relic", description = "문화재 관리 API Document")
 public class RelicController {
     @Autowired
     private ListDataRepository listDataRepository;
@@ -22,12 +25,14 @@ public class RelicController {
     private DetailDataRepository detailDataRepository;
     
     @GetMapping("/list")
+    @Operation(summary = "문화재 리스트", description = "문화재 리스트를 출력합니다.")
     public ResponseEntity<List<ListData>> getRelic() {
         List<ListData> listData = listDataRepository.findAll();
         return ResponseEntity.ok().body(listData);
     }
 
     @GetMapping("/detail/{id}")
+    @Operation(summary = "문화재 디테일", description = "문화재 상세를 출력합니다.")
     public ResponseEntity<DetailData> getDetail(@PathVariable Integer id) {
         DetailData detailData = detailDataRepository.findByListId(id);
         return ResponseEntity.ok().body(detailData);
--- BackEnd/dmobile/src/main/java/com/ssafy/dmobile/shop/controller/ShopController.java
+++ BackEnd/dmobile/src/main/java/com/ssafy/dmobile/shop/controller/ShopController.java
@@ -2,6 +2,8 @@ package com.ssafy.dmobile.shop.controller;
 
 import com.ssafy.dmobile.shop.entity.Product;
 import com.ssafy.dmobile.shop.repository.ProductRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/shop")
+@Tag(name = "Shop", description = "상점 관리 API Document")
 public class ShopController {
 
     @Autowired
     private ProductRepository productRepository;
 
     @GetMapping("/")    // 상점 들어가면 맨 처음 뜨는 물품 리스트
+    @Operation(summary = "상품 리스트", description = "상품 리스트를 출력합니다.")
     public ResponseEntity<List<Product>> productList() {
         List<Product> product = productRepository.findAll();
         return ResponseEntity.ok().body(product);
