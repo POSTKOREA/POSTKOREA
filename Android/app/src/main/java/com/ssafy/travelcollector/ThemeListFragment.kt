4c0b29f2f2bd44706ffe644a05969732460d6c77
김건우 <rlarjsdn0407@naver.com>
2024-01-31 14:02:41 +0900

    feat : 테마 즐겨찾기 기능
    - 테마 dto, adapter 추가
    - 탐방 리스트의 즐겨찾기 버튼 활성화
    - 테마 리스트 fragment 수정 중
    
    Change-Id: I6d82cfc7197e989b30b40bd2b9b39e9f7421129d

--- Android/app/src/main/java/com/ssafy/travelcollector/ThemeListFragment.kt
+++ Android/app/src/main/java/com/ssafy/travelcollector/ThemeListFragment.kt
@@ -1,59 +1,49 @@
 package com.ssafy.travelcollector
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.ssafy.travelcollector.adapter.ThemeAdapter
+import com.ssafy.travelcollector.config.BaseFragment
+import com.ssafy.travelcollector.databinding.FragmentThemeListBinding
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "ThemeListFragment"
+class ThemeListFragment : BaseFragment<FragmentThemeListBinding>(FragmentThemeListBinding::bind, R.layout.fragment_theme_list){
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ThemeListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ThemeListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val themeAdapter: ThemeAdapter by lazy{
+        ThemeAdapter()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_theme_list, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ThemeListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ThemeListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun initAdapter(){
+        lifecycleScope.launch {
+            launch {
+                mainActivityViewModel.recommendedTheme.collect{
+                    Log.d(TAG, "initAdapter: $it")
+                    themeAdapter.submitList(it)
                 }
             }
+        }
+
+        themeAdapter.clickListener = object : ThemeAdapter.ClickListener{
+            override fun onItemClick(position: Int) {
+
+            }
+
+            override fun onBookMarkClick(position: Int) {
+                val newList = mainActivityViewModel.recommendedTheme.value.toMutableList()
+                newList[position].isBookMarked = !newList[position].isBookMarked
+                mainActivityViewModel.setRecommendedTheme(newList as ArrayList)
+            }
+
+        }
+        binding.themeListRv.adapter = themeAdapter
     }
+
 }
--- Android/app/src/main/java/com/ssafy/travelcollector/viewModel/MainActivityViewModel.kt
+++ Android/app/src/main/java/com/ssafy/travelcollector/viewModel/MainActivityViewModel.kt
@@ -1,10 +1,12 @@
 package com.ssafy.travelcollector.viewModel
 
+import android.util.Log
 import androidx.collection.ArraySet
 import androidx.collection.arraySetOf
 import androidx.lifecycle.ViewModel
 import com.ssafy.travelcollector.dto.Heritage
 import com.ssafy.travelcollector.dto.Posting
+import com.ssafy.travelcollector.dto.TravelTheme
 import com.ssafy.travelcollector.dto.User
 import com.ssafy.travelcollector.test.TDto
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -50,12 +52,26 @@ class MainActivityViewModel : ViewModel() {
 
     private var _travelPlanHeritageList = MutableStateFlow(arrayListOf(TDto(1), TDto(2), TDto(3)))
     val travelPlanHeritageList = _travelPlanHeritageList.asStateFlow()
-    fun getTravelPlanHeritageList(){
-        //rest 통신 하여 각 여행의 문화재 리스트를 불러온다
+    fun loadTravelPlanHeritageList(){
+        //rest 통신을 하여 각 여행의 문화재 리스트를 불러온다
     }
 
     fun setTravelPlanHeritageList(list: ArrayList<TDto>){
         _travelPlanHeritageList.update { list }
+    }
+
+    private var _recommendedTheme = MutableStateFlow(arrayListOf(
+        TravelTheme(title = "1", isBookMarked = false),
+        TravelTheme(title = "2", isBookMarked = true),
+        TravelTheme(title = "3", isBookMarked = false)
+    ))
+    val recommendedTheme = _recommendedTheme.asStateFlow()
+    fun setRecommendedTheme(list: ArrayList<TravelTheme>){
+        _recommendedTheme.update { list }
+    }
+
+    fun loadRecommendedTheme(){
+        //rest 통신을 하여 각 여행의 문화재 리스트를 불러온다
     }
 
 }
