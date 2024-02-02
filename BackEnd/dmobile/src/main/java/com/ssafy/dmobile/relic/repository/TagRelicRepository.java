4ccdf61e4522c1c9daca8d3e197d0a78b7972038
ukyoung147 <ukyoung147@gmail.com>
2024-02-02 16:46:23 +0900

    Feat: "backend/explore_plan 개인 브랜치 병합작업"
    Entity와 DBMS간 컬럼명 통일작업 및 수정작업
    탐방 내에서 문화재 리스트 불러오는 기능 아직 미구현

--- BackEnd/dmobile/src/main/java/com/ssafy/dmobile/relic/repository/TagRelicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.dmobile.relic.repository;
-
-import com.ssafy.dmobile.relic.entity.TagRelic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRelicRepository extends JpaRepository<TagRelic, Integer> {
-}
