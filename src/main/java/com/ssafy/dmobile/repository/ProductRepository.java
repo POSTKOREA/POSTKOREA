8c84af2e2d1f93d2fe298787bfeb65f2c78aa03f
jaehee <llnmm_@nate.com>
2024-02-02 09:16:34 +0900

    feature: 문화재 목록 및 상세, DB에 저장된 상세의 컬럼 내 데이터를 파라미터로 받아 태그 검색 기능

--- src/main/java/com/ssafy/dmobile/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.dmobile.repository;
-
-import com.ssafy.dmobile.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
