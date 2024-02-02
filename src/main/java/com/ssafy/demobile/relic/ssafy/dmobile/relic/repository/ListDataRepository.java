e783d547737a4e487ca5c5ef1a687f7aef8873b3
jaehee <llnmm_@nate.com>
2024-02-02 09:24:46 +0900

    feature DB데이터 파라미터로 태그검색

---
+++ src/main/java/com/ssafy/demobile/relic/ssafy/dmobile/relic/repository/ListDataRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.dmobile.relic.repository;
+
+import com.ssafy.dmobile.relic.entity.ListData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListDataRepository extends JpaRepository<ListData, Long> {
+}
