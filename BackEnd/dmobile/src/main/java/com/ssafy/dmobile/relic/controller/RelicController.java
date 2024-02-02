1ce670d968c0de295331446e0620390186096321
jaehee <llnmm_@nate.com>
2024-02-02 17:38:12 +0900

    fix: import 추가

--- BackEnd/dmobile/src/main/java/com/ssafy/dmobile/relic/controller/RelicController.java
+++ BackEnd/dmobile/src/main/java/com/ssafy/dmobile/relic/controller/RelicController.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
