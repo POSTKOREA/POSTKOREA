2b36f0d1a5b198e09001f1498c1892667caefc6e
Lim-SeungHwan <hwan7185@gmail.com>
2024-02-06 17:41:22 +0900

    bug: 게시판 사진 등록 수정
     게시판 이미지 url수정
    
    Change-Id: I2d6388ad8b1ef9466d4452c4f7dec215c3e4ef19

---
+++ BackEnd/dmobile/src/main/java/com/ssafy/dmobile/theme/entity/ThemeKey.java
@@ -0,0 +1,18 @@
+//package com.ssafy.dmobile.theme.entity;
+//
+//import jakarta.persistence.Embeddable;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import java.io.Serializable;
+//
+//@Embeddable
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//public class ThemeKey implements Serializable {
+//
+//    private Long themeId;
+//    private Long relicId;
+//}
