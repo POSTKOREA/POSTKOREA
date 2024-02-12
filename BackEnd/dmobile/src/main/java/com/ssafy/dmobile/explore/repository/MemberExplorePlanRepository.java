c5e2b9113700dd0fcd922da0e08c236c4340ecd9
Lee-search <ukyoung147@gmail.com>
2024-02-12 22:02:33 +0900

    Fix: "외래키 참조 문제로 plan 삭제되지 않던 문제 수정"

--- BackEnd/dmobile/src/main/java/com/ssafy/dmobile/explore/repository/MemberExplorePlanRepository.java
+++ BackEnd/dmobile/src/main/java/com/ssafy/dmobile/explore/repository/MemberExplorePlanRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberExplorePlanRepository extends JpaRepository<MemberExplorePlan, MemberExplorePlanKey> {
 
     int countByKeyMemberIdAndKeyPlanId(Long memberId, Long planId);
+
+    void deleteByKeyPlanId(Long planId);
 }
